99532176118b2bb7cdb47c13bb9e9c4c1374f485
Juan David <jd.ruedat@uniandes.edu.co>
2021-10-20 16:33:12 -0500

    Controller Final

--- App/controller.py
+++ App/controller.py
@@ -27,6 +27,8 @@ import csv
 from DISClib.ADT import list as lt
 from DISClib.ADT import map as mp
 from DISClib.DataStructures import mapentry as me
+from DISClib.Algorithms.Sorting import quicksort as qck
+from DISClib.Algorithms.Sorting import shellsort as shl
 
 
 """
@@ -34,12 +36,6 @@ El controlador se encarga de mediar entre la vista y el modelo.
 """
 
 # Inicialización del Catálogo de libros
-def loadData(catalog):
-    #sortArtists(catalog)
-    #sortPieces(catalog)
-    loadinfo(catalog)
-   
-
 
 def initCatalog():
     catalog = model.newCatalog()
@@ -65,29 +61,134 @@ def loadNationality(catalog):
         for nationality in Nationalities:
             if nationality!='':
                 model.addNationality(catalog, nationality, piece)
-
-#opcion 2
-def loadMedium(catalog):
+def loadBeginDate(catalog):
+    for artist in lt.iterator(catalog['artists']):
+        begindate=artist['BeginDate']
+        if begindate != '0':
+            model.addBeginDate(catalog, begindate, artist)
+def loadDateAcquired(catalog):
     for piece in lt.iterator(catalog['pieces']):
+            DateAcquired=piece['DateAcquired']
+            if DateAcquired != '':
+                model.addDateAcquired(catalog, DateAcquired, piece)
+def loadDepartments(catalog):
+    for piece in lt.iterator(catalog['pieces']):
+        department=piece['Department']
+        if department != '':
+            model.adddepartment(catalog,piece,department)
+def loadName(catalog):
+    for piece in lt.iterator(catalog['pieces']):
+        ID = piece['ConstituentID'].replace("[",'').replace(']','')
+        if mp.contains(catalog['artistsID'], ID):
+            entry= mp.get(catalog['artistsID'], ID)
+            artist= me.getValue(entry)
+        Names=artist['DisplayName'].split(",")
+        for name in Names:
+            if name!='':
+                model.addName(catalog, name, piece)
+def loadspecificpieces(catalog, artistpieces):
+    
+    for piece in lt.iterator(artistpieces):
+        
         Medium = piece['Medium']
-        if Medium != '':
-            model.addMedium(catalog, Medium, piece)
-def result(catalog, medio, n):
-    return model.result(catalog, medio, n)
-def callcmp(date1,date2):
-    model.cmp(date1,date2)
-    return model.cmp(date1,date2)
+        if Medium != '' : #and Medium != None:
+            model.addspecificpieces(catalog, Medium, piece)
+#opcion 2
+def callbegindatesortcmp(date1,date2):
+    return model.begindatesortcmp(date1,date2)
+def callartistrangecmp(year,start,end):
+    if year != ('0') and year != (''):
+        condition=model.cmpartistrange(year,start,end)
+    else:
+        condition=False
+    return condition
+def callartistrangelist(catalog, cmpfunction,startdate,endate):
+    return model.artistrangelist(catalog, cmpfunction,startdate,endate)
+#opcion3
+def callpiecerangecmp(date, start, end):
+    if start != ('') and end != (''):
+        var= model.piecerangecmp(date,start,end)
+    else:
+        var=False
+    return var
+def callkeysinrange(catalog, cmp, start, end):
+    return model.keysinrange(catalog,cmp,start,end)
+def callkeysortcmp(key1, key2):
+    if key1 != ('') and key2 != (''):
+        condition= model.keysortcmp(key1,key2)
+    else:
+        condition=False
+    return condition
+#opcion4
+def obrasdelartista(catalog, name):
+    return model.obrasdelartista(catalog, name)
 def callgetsizemedium(catalog, medium):
     return model.getsizemedium(catalog,medium)
-#opcion3
+
+def callgetsizemediumlist(catalog):
+    return model.getsizemediumlist(catalog)
+def mediostotal(mediosespecifico):
+    return lt.size(mediosespecifico)
+
+def sortspecificpieces(catalog):
+    model.sortspecificpieces(catalog)
+def pieceinfo(catalog, medio):
+    entry = mp.get(catalog['specificpiecesmedium'], medio)
+    getval = me.getValue(entry)
+    i=0
+    res = []
+    size = lt.size(getval)
+    for piece in lt.iterator(getval):
+        if i<=3 or i>=(size - 2) :
+            restemp = ['Titulo: ' + str(piece['Title']) + ', ', 'Fecha: ' + str(piece['Date'])+ ', ', 'Medio: ' + str(piece['Medium'])+ ', ','Dimensiones: ' + str(piece['Dimensions'])+ '.']
+            res.append(restemp)
+        
+
+        i+=1
+
+    return res
+def getfirst(mediosesfecifico):
+    return lt.getElement(mediosesfecifico, 1)
+def totalobras(artistpieces):
+    return lt.size(artistpieces)
+
+#opcion5
 def callgetsizenation(catalog,nacionalidad):
     return model.getsizenation(catalog,nacionalidad)
-    
+def callsetup(catalog):
+    return model.setup(catalog)
+def callkeysort(key1,key2):
+    return model.keysort(key1,key2)
+def callnewmapnations(lst):
+    if lst != None:
+        condition=model.newmapnations(lst)
+    else:
+        condition= ('No counties found')
+    return condition
+#opcion6
+def callcost(catalog, departamento):
+    if departamento != '':
+        condition = model.cost(catalog, departamento)
+    else:
+        condition='department not found'
+    return condition
+def calldateacquiredcmp(date1, date2):
+    if date1['Date'] != '' and date2['Date'] != '':
+        condition=model.dateacquiredcmp(date1,date2)
+    else:
+        condition=False
+    return condition
+def callcostcmp(price1,price2):
+    return model.costcmp(price1,price2)
+
 def loadAll(catalog):
     loadArtists(catalog)
     loadPieces(catalog)
-    loadMedium(catalog)
     loadNationality(catalog)
+    loadBeginDate(catalog)
+    loadDateAcquired(catalog)
+    loadDepartments(catalog)
+    loadName(catalog)
 
 def loadinfo(catalog):
     artistsfile = cf.data_dir + 'Artists-utf8-small.csv'
@@ -110,6 +211,10 @@ def sortPieces(catalog):
 # Funciones de ordenamiento
 def oldpieces(catalog, medio):
     model.oldpieces(catalog, medio)
+def sortlistquick(catalog,cmpfunction):
+    return qck.sort(catalog,cmpfunction)
+def sortlistshell(catalog, cmpfunction):
+    return shl.sort(catalog,cmpfunction)
 # Funciones de consulta sobre el catálogo
 def countpieces(nacionalidad, catalog):
     return model.countpieces(nacionalidad, catalog)
